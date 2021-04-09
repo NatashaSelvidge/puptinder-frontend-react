681b7a5ca5676444fbfcc4e0b085695e0a01fba2
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-04-09 11:24:31 -0700

    added Dogs component and dog dummy data to work with
    removed state to put state in App.js

--- src/components/Dogs.js
+++ src/components/Dogs.js
@@ -1,28 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 import DogCard from "./DogCard";
-import dogs from "./dogs-data";
 
-class Dogs extends Component {
-  constructor() {
-    super();
-    this.state = {
-      dogs: [],
-    };
-  }
-  componentDidMount() {
-    //fetch dogs...
-    //.then r=>r.json
-    //.then (dogs => this.setState{(dogs: dogs)})
-    this.setState({
-      dogs: dogs,
-    });
-  }
-  render() {
-    const dogCards = this.state.dogs.map((dog) => (
-      <DogCard key={dog.id} dog={dog} />
-    ));
-    return <div>{dogCards}</div>;
-  }
+function Dogs({ dogs }) {
+  const dogCards = dogs.map((dog) => <DogCard key={dog.id} dog={dog} />);
+  
+  return <div className="Dogs">{dogCards}</div>;
 }
 
 export default Dogs;
--- src/dogs-data.js
+++ src/dogs-data.js
@@ -4,60 +4,70 @@ const dogs = [
     image_url:
       "https://images.pexels.com/photos/2304910/pexels-photo-2304910.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     name: "Mako",
+    status: "liked",
   },
   {
     id: 2,
     image_url:
       "https://images.pexels.com/photos/733416/pexels-photo-733416.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     name: "Jackson",
+    status: "liked",
   },
   {
     id: 3,
     image_url:
       "https://images.pexels.com/photos/551628/pexels-photo-551628.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     name: "Bailey",
+    status: "undecided",
   },
   {
     id: 4,
     image_url:
       "https://images.pexels.com/photos/434090/pexels-photo-434090.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     name: "Russo",
+    status: "undecided",
   },
   {
     id: 5,
     image_url:
       "https://images.pexels.com/photos/160846/french-bulldog-summer-smile-joy-160846.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     name: "Titan",
+    status: "disliked",
   },
   {
     id: 6,
     image_url:
       "https://images.pexels.com/photos/220938/pexels-photo-220938.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     name: "Britney",
+    status: "disliked",
   },
   {
     id: 7,
     image_url:
       "https://images.pexels.com/photos/773971/pexels-photo-773971.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     name: "Ralph",
+    status: "liked",
   },
   {
     id: 8,
     image_url:
       "https://images.pexels.com/photos/164186/pexels-photo-164186.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     name: "Tim",
+    status: "undecided",
   },
   {
     id: 9,
     image_url:
       "https://images.pexels.com/photos/732456/pexels-photo-732456.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     name: "Sasha",
+    status: "undecided",
   },
   {
     id: 10,
     image_url:
       "https://images.pexels.com/photos/3299899/pexels-photo-3299899.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     name: "Tiffany",
+    status: "undecided",
   },
 ];
 export default dogs;
