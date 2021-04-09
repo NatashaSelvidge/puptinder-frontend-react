020c227f3b21b20fef437395e65a0bc9aa5ddca0
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-04-09 11:51:12 -0700

    Updated App.js
    add the ability to change your mind on a dog and put their status back to undecided.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,71 @@
 import React, { Component } from "react";
-import Dogs from "./components/Dogs"
-
 import "./App.css";
+import Dogs from "./components/Dogs";
+import CenterContainer from "./components/CenterContainer";
+
+import LikedDogs from "./components/LikedDogs";
+import DislikedDogs from "./components/DislikedDogs";
+import dogs from "./dogs-data";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dogs: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      dogs: dogs,
+    });
+  }
+  //mocking fetch request
+  // componentDidMount() {
+  //   const imgURL = "https://dog.ceo/api/breeds/image/random/50";
+  //   fetch(imgURL)
+  //     .then((r) => r.json())
+  //     .then((dogs) => this.setState({ dogs: dogs }));
+  // }
+
+  handleLikeClick = (e) => {
+    //change the state of cat stutus to liked
+    let newStatus;
+    if (e.target.tagName == "IMG") {
+      newStatus = "undecided";
+    } else {
+      newStatus = e.target.className == "like-button" ? "liked" : "disliked";
+    }
+    const dogs = this.state.dogs.map((dog) => {
+      if (dog.id == e.target.id || dog.id == e.target.className) {
+        // option dog.status = "liked";
+        const newDog = Object.assign({}, dog);
+        newDog.status = newStatus;
+        return newDog;
+      } else {
+        return dog;
+      }
+    });
+    this.setState({
+      dogs: dogs,
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <h1>Welcome to Pup Tinder</h1>
-        <Dogs />
+        <LikedDogs
+          handleChangeOfHeart={this.handleLikeClick}
+          dogs={this.state.dogs.filter((dogs) => dogs.status === "liked")}
+        />
+        <CenterContainer
+          handleLikeClick={this.handleLikeClick}
+          dogs={this.state.dogs.filter((dogs) => dogs.status === "undecided")}
+        />
+        <DislikedDogs
+          handleChangeOfHeart={this.handleLikeClick}
+          dogs={this.state.dogs.filter((dogs) => dogs.status === "disliked")}
+        />
       </div>
     );
   }
